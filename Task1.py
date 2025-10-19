e84dcd500d84797146644c8a6273fdb936cba7a2
olhamanzhos <olga.manzhos.88@gmail.com>
2025-10-18 21:59:28 -0400

    Added try/catch for Task 1

--- Task1.py
+++ Task1.py
@@ -1,17 +1,23 @@
-
 def total_salary(path) -> tuple[int, float]:
-    # Open and read file
-    with open(path, encoding="utf-8") as file:
-        lines = file.readlines()
-    # Retrieve records from the file
-        payments = []
-        for line in lines:
-            name, salary = line.strip().split(",")
-            payments.append(int(salary))
-    # Calculate total and average
-        total = sum(payments)
-        average = total / len(payments)
-        return total,average
+    try:
+        # Open and read file
+        with open(path, encoding="utf-8") as file:
+            lines = file.readlines()
+            # Retrieve records from the file
+            payments = []
+            for line in lines:
+                name, salary = line.strip().split(",")
+                payments.append(int(salary))
+            # Calculate total and average
+            total = sum(payments)
+            average = total / len(payments)
+            return total, average
+    except FileNotFoundError:
+        print("File not found")
+        return 0,0
+    except Exception as e:
+        print(e)
+        return 0,0
 
-total, average = total_salary("salary_file")
+total, average = total_salary("salary_file.txt")
 print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
